e82da62cd287cdd628d2c287d107432ff881bf03
SimonRothmann <simon.rothmann@arcor.de>
2019-03-18 14:34:02 +0100

    commit du arsch

--- DHBW_Karlsruhe_Webprog-master/hauptseite.js
+++ DHBW_Karlsruhe_Webprog-master/hauptseite.js
@@ -1,14 +1,12 @@
 var myJson;
 
 function laden() {
-
-
-  var myJSON = JSON.stringify(myJson);
-  document.getElementById("demo").innerHTML = myJSON;
-}
-
-const userAction = async () => {
-  const response = await fetch('https://localhost:8443/projectAnimal/RestAPI/tierliste');
-  myJson = await response.json(); //extract JSON from the http response
-  // do something with myJson
+  fetch('http://localhost:8443/projectAnimal/RestAPI/tierliste',{ method: "GET", // *GET, POST, PUT, DELETE, etc.
+  mode: "no-cors"})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
 }
