6b4be26e06c7a13c731e4a7df4e1f263b2767172
Domenic Nauert <nauert.domenic@gmail.com>
2019-03-20 15:49:19 +0100

    Rest mit Id

--- dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/SpeziesListRest.java
+++ dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/SpeziesListRest.java
@@ -6,6 +6,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import projectanimal.whatever.ejb.SpeziesBean;
 import projectanimal.whatever.jpa.Spezies;
 
@@ -15,19 +16,20 @@ import projectanimal.whatever.jpa.Spezies;
  * @author simon
  */
 @Stateless
-@Path("speziesliste")
+@Path("speziesliste/{id}")
 public class SpeziesListRest {
 
     @EJB
     private SpeziesBean speziesBean;
 
     @GET
-    public String doGet() {
+    public String doGet(@PathParam("id") long id) {
         // Anzuzeigende Tierarten suchen
 
         List<Spezies> spezies = this.speziesBean.findAll();
+        Spezies test = this.speziesBean.findById(id);
         Gson gson = new Gson();
-        String json = gson.toJson(spezies);
+        String json = gson.toJson(test);
         System.out.println(json);
         return json;
     }
