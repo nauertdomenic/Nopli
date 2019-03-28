e445c3bc2274b5236aa69703d9127e4ac9ed9841
Domenic Nauert <nauert.domenic@gmail.com>
2019-03-28 11:59:58 +0100

    rest post

--- dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/LoginRest.java
+++ dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/LoginRest.java
@@ -1,5 +1,10 @@
 package projectanimal.whatever.rest;
 
+import com.google.gson.Gson;
+import dhbwka.wwi.vertsys.javaee.projectanimal.common.ejb.UserBean;
+import dhbwka.wwi.vertsys.javaee.projectanimal.common.jpa.User;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
 
@@ -13,30 +18,33 @@ import javax.ws.rs.*;
 @Path("loginRest")
 public class LoginRest {
 
-//    @EJB
-//    private UserBean userbean;
-//
-//    @GET
-//    public String doGet(@PathParam("un") String username, @PathParam("pw") String password) {
-//        // Login des Users
-//        List<User> userliste = this.userbean.findUser(username);
-//        String msg = "";
-//
-//        if (userliste.size() > 0) {
-//            if (userliste.get(0).checkPassword(password)) {
-//                msg = "1";
-//            } else {
-//                msg = "2";
-//            }
-//        } else {
-//            msg = "0";
-//        }
-//        Gson gson = new Gson();
-//        String json = gson.toJson(msg);
-//        return json;
-//    }
+    @EJB
+    private UserBean userbean;
+
     @POST
-    public void doPost(String inputJsonObj) throws Exception {
-        System.out.print(inputJsonObj);
+    public String doPost(String inputJsonObj) throws Exception {
+        String[] test = inputJsonObj.split("&password=");
+
+        String username = test[0].replace("username=", "");
+        String password = test[1];
+
+        //Login des Users
+        List<User> userliste = this.userbean.findUser(username);
+        String msg = "";
+
+        if (userliste.size() > 0) {
+            if (userliste.get(0).checkPassword(password)) {
+                msg = "1";
+            } else {
+                msg = "2";
+            }
+        } else {
+            msg = "0";
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(msg);
+        return json;
+
     }
 }
