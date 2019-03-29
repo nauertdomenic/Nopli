3e76adf7eedf0ef9bc80c50faa76bb03f9209eb5
Domenic Nauert <nauert.domenic@gmail.com>
2019-03-29 11:13:39 +0100

    rest

--- dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/SpeziesListRest.java
+++ dhbwka-wwi-vertsys-nopli-master/src/main/java/projectanimal/whatever/rest/SpeziesListRest.java
@@ -37,13 +37,10 @@ public class SpeziesListRest {
             // credentials = username:password
             final String[] values = credentials.split(":", 2);
 
-            System.out.println("");
-            System.out.println(values[0] + " bla " + values[1]);
-
             // Error: 0 = kein User; 2 = falsches Passwort
             List<User> user = userBean.findUser(values[0]);
             if (user.size() < 1) {
-                return "0";
+                return "3";
             }
 
             if (user.get(0).checkPassword(values[1])) {
